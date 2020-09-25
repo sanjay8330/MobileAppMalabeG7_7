b9a8dffb93a90a284ceae426d0cea9d793579386
User <chalanajanith12341@gmail.com>
2020-09-25 04:14:05 -0700

    initial commit

---
+++ app/src/main/java/com/example/it19151434project/Login_Activity.java
@@ -0,0 +1,121 @@
+package com.example.it19151434project;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+
+public class Login_Activity extends AppCompatActivity {
+    private EditText userloginName;
+    private EditText userloginpassword;
+    private TextView usercreateAcc;
+    private TextView userforgotpsw;
+    private Button userloginButton;
+    private ProgressDialog userprogress;
+    private DatabaseReference dbref;
+
+    private FirebaseAuth mAuth;
+
+    String emailpattern = "[a-zA-z0-9._=]+@[a-z]+\\.+[a-z]+";
+
+    private void clearControls(){
+        userloginName.setText("");
+        userloginpassword.setText("");
+
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_);
+
+        userprogress = new ProgressDialog(this);
+        mAuth = FirebaseAuth.getInstance();
+
+        if (mAuth.getCurrentUser() != null) {
+
+            // startActivity(new Intent(getApplicationContext(),Profile.class));
+        }
+        userlogin();
+    }
+    private  void userlogin(){
+        userloginName = findViewById(R.id.login_yourname);
+        userloginpassword = findViewById(R.id.login_password);
+        userloginButton = findViewById(R.id.Login_Btn);
+        userforgotpsw = findViewById(R.id.forgot_Password);
+
+        userloginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final String LoginEmail = userloginName.getText().toString().trim();
+                final String PasswordLogin = userloginpassword.getText().toString().trim();
+
+                if (TextUtils.isEmpty(LoginEmail)){
+                    userloginName.setError("Email is Required");
+                    userloginName.requestFocus();
+                    return;
+                }
+                if(!LoginEmail.matches(emailpattern)){
+                    userloginName.setError("invalid email pattern");
+                    userloginName.requestFocus();
+                    return;
+
+                } if (TextUtils.isEmpty(PasswordLogin)) {
+                    userloginpassword.setError("Password is Required");
+                    userloginpassword.requestFocus();
+                    return;
+                }
+
+                userprogress.setMessage("Login in...");
+                userprogress.show();
+
+
+                mAuth.signInWithEmailAndPassword(LoginEmail,PasswordLogin).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        if(task.isSuccessful()){
+                            userprogress.dismiss();
+                            clearControls();
+                            Toast.makeText(getApplicationContext(),"Welcome.....\nTour Assist Sri Lanka",Toast.LENGTH_LONG).show();
+                            Intent intent = new Intent(Login_Activity.this,UserProfile.class);
+                            intent.putExtra("id",LoginEmail);
+                            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                            startActivity(intent);
+                        }else{
+                            userprogress.dismiss();
+                            Toast.makeText(getApplicationContext(),"login failed",Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+            }
+        });
+
+        userforgotpsw.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                  Intent intent4 = new Intent(Login_Activity.this,UserForgetPassword.class);
+                 intent4.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                  intent4.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                 startActivity(intent4);
+            }
+        });
+
+    }
+}
