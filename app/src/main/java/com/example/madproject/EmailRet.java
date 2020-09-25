6f265eb1a3a7854ce7c288902f1cd02d4bc79553
induwara99 <Induwara0399@gmail.com>
2020-09-25 15:54:29 +0530

    Initial Commit 09/25/2020

--- app/src/main/java/com/example/madproject/EmailRet.java
+++ app/src/main/java/com/example/madproject/EmailRet.java
@@ -1,10 +1,10 @@
 package com.example.madproject;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
--- app/src/main/java/com/example/madproject/PaypalPay.java
+++ app/src/main/java/com/example/madproject/PaypalPay.java
@@ -10,7 +10,6 @@ import android.widget.EditText;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
-
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
